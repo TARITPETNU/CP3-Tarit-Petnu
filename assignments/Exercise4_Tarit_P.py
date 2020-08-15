4d0d112e4da8fab59be45188fa4cdbb0d0fd1e45
TARITPETNU <56666174+TARITPETNU@users.noreply.github.com>
2020-08-15 14:19:22 +0700

    Add files via upload

---
+++ assignments/Exercise4_Tarit_P.py
@@ -0,0 +1,10 @@
+Score_FoundationEnglish = 99.99
+Score_GeneralBusiness = 98.88
+Score_IntroductiontoComputerSystems = 97.77
+Score_ComputerProgramming = 96.66
+
+print("--- Your Score ---")
+print("Foundation English : ", Score_FoundationEnglish)
+print("General Business : ",  Score_GeneralBusiness)
+print("Introduction to Computer Systems : ", Score_IntroductiontoComputerSystems)
+print("Computer Programming : ", Score_ComputerProgramming)
